aff27e9a593fc8c8e3e0a56b6fb778c856fddde1
Kiki Diki <dicol02@mail.ru>
2023-09-06 11:56:29 +0300

    jetbrains test2

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('  ssss   ')
+    print_hi('  asdasd   ')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
