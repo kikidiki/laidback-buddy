c4bc7ce1dc38c98d4de938ec1df03f4c19ad3f71
Kiki Diki <dicol02@mail.ru>
2023-09-12 17:13:29 +0300

    Added auto queue accept

--- asd.py
+++ asd.py
@@ -2,6 +2,7 @@ import time
 import urllib3
 from vars import *
 import threading
+import pywinauto
 
 import os
 
@@ -15,6 +16,22 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 # Create the connector object
 connector = lcu_driver.Connector()
 # Define a function to be called when LCU API is ready to be used
+
+p = subprocess.Popen(["D:\\Games\\Riot Games\\Riot Client\\RiotClientServices.exe", "--headless",
+                      "--launch-product=league_of_legends", "--launch-patchline=live"])
+
+while True:
+    try:
+        # Try to find the lol client window by its title
+        app = pywinauto.Application().connect(title="League of Legends", found_index=0)
+        print("The lol client is loaded and ready.")
+        time.sleep(1)
+
+        break
+    except pywinauto.findwindows.ElementNotFoundError:
+        # The window is not found, so the lol client is still loading
+        print("The lol client is still loading...")
+        time.sleep(1)
 @connector.ready
 async def connect(connection):
 
@@ -22,6 +39,11 @@ async def connect(connection):
     await connection.request('post', '/lol-lobby/v2/lobby', data={'queueId': 420})
     # Start matchmaking
     await connection.request('post', '/lol-lobby/v2/lobby/matchmaking/search')
+
+# @connector.ws.register('/lol-matchmaking/v1/ready-check', event_types=('UPDATE',))
+# async def ready_check_changed(connection, event):
+#     if event.data['state'] == 'InProgress' and event.data['playerResponse'] == 'None':
+#         await connection.request('post', '/lol-matchmaking/v1/ready-check/accept', data={})
 
 
 # Register the connect function to be called on startup
--- asd2.py
+++ asd2.py
@@ -1,4 +1,8 @@
 import time
+import lcu_driver
+import asyncio
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 from vars import *
 import os
@@ -6,30 +10,26 @@ import os
 connector = lcu_driver.Connector()
 
 async def launch_lol():
-    p = subprocess.Popen(["D:\Games\Riot Games\Riot Client\RiotClientServices.exe", "--headless",
+    p = subprocess.Popen(["D:\\Games\\Riot Games\\Riot Client\\RiotClientServices.exe", "--headless",
                           "--launch-product=league_of_legends", "--launch-patchline=live"])
 
-async def create_lobby(connection):
-    await connection.request('post', '/lol-lobby/v2/lobby', data={'queueId': 420})
-    await connection.request('post', '/lol-lobby/v2/lobby/matchmaking/search')
 
 @connector.ws.register('/lol-service-status/v1/lcu-status', event_types=('UPDATE',))
-async def client_status_changed(connection, event):
+async def ready_check_changed(connection, event):
     if event.data['connectionState'] == 'Connected':
-        task1 = asyncio.create_task(launch_lol())
-        task2 = asyncio.create_task(create_lobby())
-        await task1
-        await task2
-
-@connector.ready
-async def connect(connection):
-    pass
-
-connector.start()
-
+        await connection.request('post', '/lol-lobby/v2/lobby', data={'queueId': 420})
+        await connection.request('post', '/lol-lobby/v2/lobby/matchmaking/search')
 
 
 @connector.ws.register('/lol-matchmaking/v1/ready-check', event_types=('UPDATE',))
 async def ready_check_changed(connection, event):
     if event.data['state'] == 'InProgress' and event.data['playerResponse'] == 'None':
         await connection.request('post', '/lol-matchmaking/v1/ready-check/accept', data={})
+
+
+connector.start()
+
+
+
+
+asyncio.run(launch_lol())
